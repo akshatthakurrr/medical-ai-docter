eb5e8c7cf92f36507966fdf05614a4acb509ed53
akshatthakurrr <161508422+akshatthakurrr@users.noreply.github.com>
2025-08-07 02:32:31 +0530

    Update brain.py

--- brain.py
+++ brain.py
@@ -2,7 +2,7 @@ import base64
 from groq import Groq
 import os
 
-client = Groq(api_key="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")
+client = Groq(api_key="your api key")
 
 def encode_image(image_path):
     with open(image_path, "rb") as image_file:
@@ -26,3 +26,4 @@ def analyze_image_with_query(query, encoded_image, model="meta-llama/llama-4-mav
         model=model
     )
     return chat_completion.choices[0].message.content
+
